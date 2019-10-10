144b36a03ef62edb1a9b8e0f41145c3939aeb36e
Sam Kingdon <smarky55@gmail.com>
2019-10-10 20:23:46 +0100

    Update and tidy up

--- AutoThrottle/Performance.h
+++ AutoThrottle/Performance.h
@@ -1,6 +1,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0.If a copy of the MPL was not distributed with this
-// file, You can obtain one at http ://mozilla.org/MPL/2.0/.
+// License, v.2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #pragma once
 
@@ -14,7 +14,6 @@
 
 #include "XPLM/XPLMDataAccess.h"
 
-#include "util.h"
 
 enum DrefFlag {
 	DrefFlag_IsArray = 1,
--- AutoThrottle/ap_core.cpp
+++ AutoThrottle/ap_core.cpp
@@ -1,8 +1,14 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0.If a copy of the MPL was not distributed with this
-// file, You can obtain one at http ://mozilla.org/MPL/2.0/.
+// License, v.2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "ap_core.h"
+
+#include <XPLM/XPLMDataAccess.h>
+#include <XPLM/XPLMProcessing.h>
+#include <XPLM/XPLMUtilities.h>
+
+#include "util.h"
 
 // GLOBALS
 
